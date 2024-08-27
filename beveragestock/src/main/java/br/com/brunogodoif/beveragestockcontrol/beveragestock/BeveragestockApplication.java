fbebf46066f03b450cc712732a934f25f7bbf7fa
brunogodoif <brunofgodoi@outlook.com.br>
2024-08-27 18:57:03 -0300

    Create project

---
+++ beveragestock/src/main/java/br/com/brunogodoif/beveragestockcontrol/beveragestock/BeveragestockApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunogodoif.beveragestockcontrol.beveragestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeveragestockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeveragestockApplication.class, args);
+    }
+
+}
---
+++ beveragestock/src/test/java/br/com/brunogodoif/beveragestockcontrol/beveragestock/BeveragestockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunogodoif.beveragestockcontrol.beveragestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeveragestockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
